ed539fe16213b89ff6a65c4d797739e62575f494
pantarat <pantarat@localhost>
2024-02-11 02:47:38 +0700

    Fixed u,l

--- SSA.m
+++ SSA.m
@@ -1,3 +1,62 @@
 [audioData,fs] = audioread("Audio_Source/sample_data.wav");
-soundsc(audioData,fs);
-audioData
+
+% Define variables
+l = 200;
+u = 400;
+b = 1;
+
+N = length(audioData);
+L = 1024;
+K = N - L + 1;
+X = zeros(L,K);
+for i = 1:K
+    X(:,i) = audioData(i:L+i-1);
+end
+[U,D,V] = svd(X,"vector","econ");
+
+plot(D,"o");
+saveas(gcf,'./Plot_Output/Original.png')
+
+% Modify
+if b == 1
+    modified = D(l);
+else
+    modified = D(u);
+end
+D(l:u) = modified;
+plot(D,"o");
+saveas(gcf,'./Plot_Output/Modified.png')
+
+% Reassemble
+Y = U*diag(D)*V';
+output = zeros(N,1);
+
+Ls = size(X,1);
+Ks = N - Ls + 1;
+
+% Hankelize
+for k = 1:Ls-1
+    sum = 0;
+    for m = 1:k
+        sum = sum + Y(m,k-m+1);
+    end
+    output(k) = (1/k)*sum;
+end
+for k = Ls:Ks
+    sum = 0;
+    for m = 1:Ls-1
+        sum = sum + Y(m,k-m+1);
+    end
+    output(k) = (1/Ls-1)*sum;
+end
+for k = Ks+1:N
+    sum = 0;
+    for m = k-Ks+1:N-Ks+1
+        sum = sum + Y(m,k-m+1);
+    end
+    output(k) = (1/(N-k+1))*sum;
+end
+
+% Write Output
+output = normalize(output, 'range', [-1 1]);
+audiowrite("Audio_Output/Sample1.wav",output,fs);
